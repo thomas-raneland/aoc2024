48a4df824c92d65f8fa546750043fdf1197b8437
Thomas Raneland <thomas.raneland@voyado.com>
2024-12-01 06:46:11 +0100

    Add template

---
+++ src/Day00.java
@@ -0,0 +1,18 @@
+public class Day00 {
+    private static final String INPUT = """
+            """;
+
+    public static void main(String... args) {
+        partI();
+        partII();
+    }
+
+    private static void partI() {
+        var res = 0;
+        System.out.println("Part I: " + res);
+    }
+
+    private static void partII() {
+        // TODO
+    }
+}
